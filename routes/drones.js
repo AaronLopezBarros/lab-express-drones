e57e6191df40cbc7276cdd52f2e29c6e37a4d479
Aarón López <n_aaron5@hotmail.com>
2021-11-17 20:26:04 +0100

    iteration 4 done

--- routes/drones.js
+++ routes/drones.js
@@ -5,6 +5,7 @@ const chalk   = require('chalk')
 const Drone = require('../models/Drone.model')
 
 router.get('/drones', async (req, res, next) => {
+  // Iteration #2: List the drones
   try{
       const drones = await Drone.find()
       console.log(drones)
@@ -16,29 +17,45 @@ router.get('/drones', async (req, res, next) => {
 });
 
 router.get('/drones/create', (req, res, next) => {
+  // Iteration #3: Add a new drone
   res.render('drones/create-form')
 });
 
 router.post('/drones/create', async (req, res, next) => {
+  // Iteration #3: Add a new drone
   try{
     const {name, propellers, maxSpeed} = req.body
     const createDrone = await Drone.create({name, propellers, maxSpeed})
-    console.log(chalk.bgYellow(createDrone))
     res.redirect('/drones')
 
   } catch(err){
     console.log(chalk.bgRed(err))
+    res.render('/drones/create')
   }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try{
+    const drone = await Drone.findById(req.params.id)
+    res.render('../views/drones/update-form.hbs', {drone})
+  } catch(err){
+    console.log(chalk.bgRed(err))
+  }
+  
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {console.log('ok')
   // Iteration #4: Update the drone
-  // ... your code here
+  try{
+    const {name, propellers, maxSpeed} = req.body
+    const updateDrone = await Drone.findByIdAndUpdate(req.params.id, {name, propellers, maxSpeed})
+    res.redirect('/drones')
+  } catch(err){
+    console.log(chalk.bgRed(err))
+    res.render('../views/drones/update-form.hbs')
+  }
+  
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
